de21a1d526e7721f47ab232a71261f41e04fe24b
Diego Marino <marinodiego.96@gmail.com>
2022-03-28 14:18:19 +0200

    change result appearance

--- src/components/calculator/Result.js
+++ src/components/calculator/Result.js
@@ -1,16 +1,15 @@
-import { Box, Button, Divider, Flex, Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
 function Result(props) {
   return (
-    <Flex
-      boxShadow='2xl'
-      border='1px'
-      borderColor='blue.50'
-      bg='white'
-      className='p-5 m-10 result rounded-2xl items-center space-x-5 justify-center'>
-      <Heading size='xl'> Result: </Heading>
-      <Text fontSize='4xl'>{props.sum}</Text>
+    <Flex boxShadow='2xl' bg='#057bff' className='p-5 m-10 result rounded-2xl items-center space-x-5 justify-center'>
+      <Heading color='white' size='xl'>
+        Result:
+      </Heading>
+      <Text color='white' fontSize='4xl'>
+        {props.sum}
+      </Text>
     </Flex>
   );
 }
